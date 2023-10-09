e2cf841f4792dc2e79bd070671d02490177eb94e
Zachary Kuchar <zacharyk323@live.com>
2023-10-09 10:08:54 -0400

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import logo from "./Images/PlayingCards4.jpg";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} alt="" />
+      <Navbar />
     </div>
   );
 }
