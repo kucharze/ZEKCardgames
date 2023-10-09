8e6a2dc1db96010bfa106bc2a752679c8f773778
Zachary Kuchar <zacharyk323@live.com>
2023-10-09 10:00:42 -0400

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import logo from "./Images/PlayingCards4.jpg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={logo} alt="" />
+    </div>
+  );
 }
 
 export default App;
