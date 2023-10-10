49cd2399c1a390b13ad7bddf36a309ccd048eec0
Zachary Kuchar <zacharyk323@live.com>
2023-10-10 16:00:51 -0400

    Filled out options for dropdown in Leaderboard form

--- src/pages/Leaderboards/Leaderboards.jsx
+++ src/pages/Leaderboards/Leaderboards.jsx
@@ -4,10 +4,17 @@ function Leaderboards() {
   return (
     <div>
       <h1>Leaderboards</h1>
+      <h2>Select a game to bring up the leaderboards relating to it.</h2>
       <form>
         <select>
+            <option>Select a leaderboard option</option>
             <option>Crazy Eights</option>
             <option>Blackjack</option>
+            <option>Snip Snap Snorum</option>
+            <option>Matching</option>
+            <option>Go Fish</option>
+            <option>Spider Solitare</option>
+            <option>War</option>
         </select>
         <input type='submit' />
       </form>
