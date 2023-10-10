890ddc1aed6ccf7b1fefa7ec4249e5c24af8a93c
Zachary Kuchar <zacharyk323@live.com>
2023-10-10 14:37:24 -0400

    Added suggestions page

---
+++ src/pages/Suggestions/Suggestions.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Suggestions() {
+  return (
+    <div>
+      <h1>Suggesstions</h1>
+      <h3>If you have any suggestions you would like to make or would like to report a bug
+        You may submit them down below.
+      </h3>
+    </div>
+  )
+}
+
+export default Suggestions
