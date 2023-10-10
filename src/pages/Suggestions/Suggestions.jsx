b1d34f939d2eb3d7b18a8c9e6cf7e1217e136f7f
Zachary Kuchar <zacharyk323@live.com>
2023-10-10 14:47:34 -0400

    Suggestion form

--- src/pages/Suggestions/Suggestions.jsx
+++ src/pages/Suggestions/Suggestions.jsx
@@ -7,6 +7,10 @@ function Suggestions() {
       <h3>If you have any suggestions you would like to make or would like to report a bug
         You may submit them down below.
       </h3>
+      <form>
+        <input placeholder='Put your suggestions here'/><br/>
+        <input type='submit'/>
+      </form>
     </div>
   )
 }
