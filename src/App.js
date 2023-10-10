9435d53421d961e5a0f9fbba90d9e3a604443b84
Zachary Kuchar <zacharyk323@live.com>
2023-10-10 14:28:56 -0400

    Import of gamenavbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./Images/PlayingCards4.jpg";
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Accounts from "./pages/Accounts/Accounts";
+import GameNavbar from "./components/Navbar/GameNavbar";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} alt="" />
       <Navbar />
+      <GameNavbar />
       <Routes>
         <Route path="/" element={<Accounts />} />
       </Routes>
