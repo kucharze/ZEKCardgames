5a86baec58f08f3d9a8ddbd8e88ffeb4d8622d3a
Zachary Kuchar <zacharyk323@live.com>
2023-10-10 11:48:36 -0400

    Linked accounts page to main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import logo from "./Images/PlayingCards4.jpg";
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Accounts from "./pages/Accounts/Accounts";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} alt="" />
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Accounts />} />
+      </Routes>
     </div>
   );
 }
