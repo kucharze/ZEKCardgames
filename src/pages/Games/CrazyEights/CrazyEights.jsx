2db928854e202045515e27c254a3278f598b90dd
Zachary Kuchar <zacharyk323@live.com>
2023-10-12 15:44:41 -0400

    Added new elements and styles to Crazy EIghts

--- src/pages/Games/CrazyEights/CrazyEights.jsx
+++ src/pages/Games/CrazyEights/CrazyEights.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './CrazyEights.module.css'
 
 function CrazyEights() {
   return (
-    <div>
+    <div className={styles.CrazyEights}>
       <h1>Crazy Eights</h1>
+      <h2>Computer</h2>
+      <div className='com'></div>
+      <div>The display section for the suit</div>
+      <div className='table'>deck,,, pile</div>
+      <h2>Player</h2>
     </div>
   )
 }
