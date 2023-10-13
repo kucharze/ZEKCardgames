589c7c1fd94cab083385013bea76b85b51286183
Zachary Kuchar <zacharyk323@live.com>
2023-10-13 15:35:40 -0400

    Additional adjustments to display cards correctly

--- src/pages/Games/CrazyEights/CrazyEights.jsx
+++ src/pages/Games/CrazyEights/CrazyEights.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect, useRef} from 'react'
 import styles from './CrazyEights.module.css'
-import p from '../Images/10c.png'
+// import p from '../Images/10c.png'
 import back from '../../../Images/back.png'
 
 function CrazyEights() {
-  const [playHand, setPlayHand] = useState([])
-  const [comHand, setComHand] = useState(['10c'])
+  const [playHand, setPlayHand] = useState(['2h','10c','10c','10c','10c'])
+  const [comHand, setComHand] = useState(['10c','10c','10c','10c'])
+  const [pile,setPile] = useState('3h')
   const imageRef = useRef('../../../Images/2c.png');
 
   const cardPicked = () =>{
@@ -19,15 +20,19 @@ function CrazyEights() {
       <div className='com'>{
         comHand.map((item,i)=>{
           console.log(item)
-          return <img key={i} src={require(`../../../Images/${item}.png`)} alt="" ref={imageRef} />
+          return <img className={styles.hand} key={i} src={back} alt="" ref={imageRef} />
         })
       }</div>
       <div>The display section for the suit</div>
       <div className='table'>
-        <img src={back} alt="" onClick={cardPicked} />,,, pile</div>
+        <img className={styles.hand} src={back} alt="" onClick={cardPicked} />,
+         <img className={styles.hand} src={require(`../../../Images/${pile}.png`)} alt="" ref={imageRef} /></div>
       <h2>Player</h2>
       <div className='player'>{
-        
+        playHand.map((item,i)=>{
+          console.log(item)
+          return <img className={styles.hand} key={i} src={require(`../../../Images/${item}.png`)} alt="" ref={imageRef} />
+        })
       }</div>
     </div>
   )
