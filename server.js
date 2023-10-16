b6c64e95285a1fd6c1449a1147d85dae787c2b3d
Zachary Kuchar <zacharyk323@live.com>
2023-10-16 14:24:08 -0400

    Mongoose setup plus changes to allow for it to work

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 
+const app = express();
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 
-const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE);
+
+const db = mongoose.connection;
+
+db.on("connected", function () {
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+});
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
