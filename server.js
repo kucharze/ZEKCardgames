eec04ba017ba8b1550a907123cbb4369c46240f7
Zachary Kuchar <zacharyk323@live.com>
2023-10-16 11:39:58 -0400

    Set up for server file

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
+require("dotenv").config();
+
+const cors = require("cors");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
 
 const app = express();
 
