2e7caa9f8f9c3b21489d15a7cfadb9c494609cb9
Zachary Kuchar <zacharyk323@live.com>
2023-10-23 11:32:29 -0400

    Additional assests for setting up a new game

--- src/pages/Games/CrazyEights/CrazyEights.jsx
+++ src/pages/Games/CrazyEights/CrazyEights.jsx
@@ -7,9 +7,12 @@ import ComputerPlayer from '../../../gamecomponents/ComputerPlayer'
 import Card from '../../../gamecomponents/Card'
 import Deck from '../../../gamecomponents/Deck'
 
+
 function CrazyEights() {
   const [playHand, setPlayHand] = useState(['2h','10c','10c','10h'])
   const [comHand, setComHand] = useState(['10c','10c','10c','10c'])
+  const [player, setPlayer] = useState(null)
+  const [com, setCom] = useState(null)
   const [pile,setPile] = useState('3h')
   const [deck, setDeck] = useState(new Deck())
   const imageRef = useRef('../../../Images/2c.png');
@@ -19,7 +22,13 @@ function CrazyEights() {
   }
 
   const newGame = () =>{
+    deck.shuffle()
+    deck.shuffle()
     console.log("Start a new game")
+    setPlayHand([deck.dealACard(),deck.dealACard(),deck.dealACard(),deck.dealACard(),])
+    setComHand([deck.dealACard(),deck.dealACard(),deck.dealACard(),deck.dealACard(),])
+    console.log(deck)
+    console.log("hello")
   }
 
   useEffect(()=>{
@@ -33,7 +42,7 @@ function CrazyEights() {
       <h2>Computer</h2>
       <div className='com'>{
         comHand.map((item,i)=>{
-          return <img className={styles.hand} key={i} src={back} alt="" ref={imageRef} />
+          return <img className={styles.hand} key={i} src={require(`../../../Images/${item}.png`)} alt="" ref={imageRef} />
         })
       }</div>
       <div>The display section for the suit</div>
