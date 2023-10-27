813e44ed9f7f4bf2148bcb5fa9836a23013e0215
Zachary Kuchar <zacharyk323@live.com>
2023-10-27 15:09:31 -0400

    Fix for setting the suit

--- src/components/SuitPicker/SuitPicker.jsx
+++ src/components/SuitPicker/SuitPicker.jsx
@@ -5,10 +5,10 @@ function SuitPicker({func}) {
   return (
     <div className={styles.SuitPicker}>
       <h2>Choose the new suit</h2>
-      <button onClick={()=>func('Diamonds')}>Diamonds</button>
-      <button onClick={()=>func('Hearts')}>Hearts</button>
-      <button onClick={()=>func('Clubs')}>Clubs</button>
-      <button onClick={()=>func('Spades')}>Spades</button>
+      <button onClick={()=>func('c')}>Diamonds</button>
+      <button onClick={()=>func('d')}>Hearts</button>
+      <button onClick={()=>func('h')}>Clubs</button>
+      <button onClick={()=>func('s')}>Spades</button>
     </div>
   )
 }
