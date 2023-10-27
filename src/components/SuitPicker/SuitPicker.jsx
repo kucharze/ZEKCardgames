3b64e6a175277a1c415011e215f929bf154c353f
Zachary Kuchar <zacharyk323@live.com>
2023-10-27 12:26:57 -0400

    Added buttons for suites

--- src/components/SuitPicker/SuitPicker.jsx
+++ src/components/SuitPicker/SuitPicker.jsx
@@ -5,6 +5,10 @@ function SuitPicker({func}) {
   return (
     <div className={styles.SuitPicker}>
       <h1>Ths suit picker for Crazy eights</h1>
+      <button>Diamonds</button>
+      <button>Hearts</button>
+      <button>Clubs</button>
+      <button>Spades</button>
     </div>
   )
 }
