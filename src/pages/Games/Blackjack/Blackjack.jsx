f8c3cc8e4d6f0498fb5609196288a1ce8991b9a9
Zachary Kuchar <zacharyk323@live.com>
2023-11-28 19:26:10 -0500

    Initail state for blackjack values

--- src/pages/Games/Blackjack/Blackjack.jsx
+++ src/pages/Games/Blackjack/Blackjack.jsx
@@ -2,6 +2,9 @@ import React,{useState, useEffect} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
 
 function Blackjack() {
+  const [value, setValue] = useState(0)
+  const [comValue, setComValue] = useState(0)
+
   return (
     <div>
       <h1>Blackjack</h1>
