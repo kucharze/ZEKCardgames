94e36a6a3969122543d4e0ace4072949079b68f5
Zachary Kuchar <zacharyk323@live.com>
2023-11-28 20:14:11 -0500

    Changes to styles

--- src/pages/Games/Blackjack/Blackjack.jsx
+++ src/pages/Games/Blackjack/Blackjack.jsx
@@ -1,13 +1,18 @@
 import React,{useState, useEffect} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
+import styles from './Blackjack.module.css'
 
 function Blackjack() {
   const [value, setValue] = useState(0)
   const [comValue, setComValue] = useState(0)
 
   return (
-    <div>
+    <div className={styles.Blackjack}>
       <h1>Blackjack</h1>
+      <h2>Com</h2>
+      <img src={back} alt="" />
+      <h2>Player</h2>
+      <img src={back} alt="" />
     </div>
   )
 }
