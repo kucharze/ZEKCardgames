badb022fae533cad15f634833eece7b66fe63a19
Zachary Kuchar <zacharyk323@live.com>
2023-11-28 19:22:53 -0500

    Initial setup for snip snap snorum section

--- src/pages/Games/SnipSnapSnorum/SnipSnapSnorum.jsx
+++ src/pages/Games/SnipSnapSnorum/SnipSnapSnorum.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import back from '../../../Images/BACKCARD.JPG'
 
 function SnipSnapSnorum() {
   return (
     <div>
       <h1>Snip Snap Snorum</h1>
+      <h1>Com</h1>
+      <img src={back} alt="" />
+      <h1>Pile</h1>
+      <img src={back} alt="" />
+      <h1>Player</h1>
+      <img src={back} alt="" />
     </div>
   )
 }
