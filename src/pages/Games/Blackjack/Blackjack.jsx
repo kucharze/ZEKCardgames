570323582069cae8b70bd6885a5c1ee775d4ffc2
Zachary Kuchar <zacharyk323@live.com>
2023-11-29 13:33:42 -0500

    Initial states to blackjack

--- src/pages/Games/Blackjack/Blackjack.jsx
+++ src/pages/Games/Blackjack/Blackjack.jsx
@@ -1,11 +1,19 @@
 import React,{useState, useEffect} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
 import styles from './Blackjack.module.css'
+import Deck from '../../../gamecomponents/Deck'
+import SuitPicker from '../../../components/SuitPicker/SuitPicker'
+import WinnerBoard from '../../../components/Winnerboard/WinnerBoard'
 
 function Blackjack() {
+  const [playHand, setPlayHand] = useState([])
+  const [comHand, setComHand] = useState([])
   const [value, setValue] = useState(0)
   const [comValue, setComValue] = useState(0)
-
+  const [deck, setDeck] = useState(new Deck())
+  const [gameOver, setGameOver] = useState(false)
+  const [youWin, setYouWin] = useState(false)
+  
   return (
     <div className={styles.Blackjack}>
       <h1>Blackjack</h1>
