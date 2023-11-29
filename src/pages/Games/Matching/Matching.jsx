b6eed991be60447e54f3b593f581ed38691fc436
Zachary Kuchar <zacharyk323@live.com>
2023-11-29 11:11:41 -0500

    Initial setup and styles for Matching

--- src/pages/Games/Matching/Matching.jsx
+++ src/pages/Games/Matching/Matching.jsx
@@ -1,9 +1,34 @@
 import React,{useState, useEffect} from 'react'
+import back from '../../../Images/BACKCARD.JPG'
+import styles from './Matching.module.css'
 
 function Matching() {
   return (
-    <div>
+    <div className={styles.Matching}>
       <h2>Matching</h2>
+      <div className={styles.matchzone}>
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+        <img src={back} alt="" />
+      </div>
+
     </div>
   )
 }
