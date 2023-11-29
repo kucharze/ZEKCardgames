609517d67d94624e350b8c30025ba5207d63556d
Zachary Kuchar <zacharyk323@live.com>
2023-11-29 09:35:24 -0500

    Fixed setup for winnerboard display

--- src/components/Winnerboard/WinnerBoard.jsx
+++ src/components/Winnerboard/WinnerBoard.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import styles from './WinnerBoard.module.css'
 
-function WinnerBoard(winner) {
+function WinnerBoard({winner}) {
   return (
     <div className={styles.WinnerBoard}>
-      {(winner == "Player") && <h1>You win</h1>}
+      {winner && <h1>You win</h1>}
+      {(!winner) && <h1>You lose</h1>}
       {/* <h2>Play again?</h2>
       <button>Yes</button>
       <button>No</button> */}
