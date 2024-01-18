c4f12daf879261cc4aa2604209e7ebe2465b24b0
Zachary Kuchar <zacharyk323@live.com>
2024-01-18 13:35:50 -0500

    Initial setup for Spider Solitare

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import back from '../../../Images/BACKCARD.JPG'
+import Deck from '../../../gamecomponents/Deck'
 
 function SpiderSolitare() {
   return (
     <div>
       <h1>Spider Solitare</h1>
+      <div><img src={back} alt="" /></div>
+      <div><img src={back} alt="" /></div>
+      <div><img src={back} alt="" /></div>
+      <div><img src={back} alt="" /></div>
+      <div><img src={back} alt="" /></div>
     </div>
   )
 }
