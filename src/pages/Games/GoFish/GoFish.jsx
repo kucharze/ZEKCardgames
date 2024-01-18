bb1779a04f9307c84a022b2296bc41ad41d4d5cc
Zachary Kuchar <zacharyk323@live.com>
2024-01-18 10:48:36 -0500

    Adding imports

--- src/pages/Games/GoFish/GoFish.jsx
+++ src/pages/Games/GoFish/GoFish.jsx
@@ -1,14 +1,17 @@
 import React,{useState, useEffect} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
+import Deck from '../../../gamecomponents/Deck'
+import styles from './GoFish.module.css'
+
 
 function GoFish() {
   return (
-    <div>
+    <div className={styles.GoFish}>
       <h1>Go Fish</h1>
       <h2>Com</h2>
-      <img src={back} alt="" />
+      <img className={styles.card} src={back} alt="" />
       <h2>Player</h2>
-      <img src={back} alt="" />
+      <img className={styles.card} src={back} alt="" />
     </div>
   )
 }
--- src/pages/Games/SnipSnapSnorum/SnipSnapSnorum.jsx
+++ src/pages/Games/SnipSnapSnorum/SnipSnapSnorum.jsx
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react'
-import back from '../../../Images/BACKCARD.JPG'
 import styles from './SnipSnapSnorum.module.css'
+import back from '../../../Images/BACKCARD.JPG'
 import Deck from '../../../gamecomponents/Deck'
 
 function SnipSnapSnorum() {
