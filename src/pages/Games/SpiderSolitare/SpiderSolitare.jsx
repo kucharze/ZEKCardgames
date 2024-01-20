4a24d381e757b3ae094399c17234fe757904e9d3
Zachary Kuchar <zacharyk323@live.com>
2024-01-20 12:11:23 -0500

    Styles for spidersolitare

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
 import Deck from '../../../gamecomponents/Deck'
+import styles from './Spidersolitare.module.css'
 
 function SpiderSolitare() {
   return (
-    <div>
+    <div className={styles.SpiderSolitare}>
       <h1>Spider Solitare</h1>
-      <div><img src={back} alt="" /></div>
-      <div><img src={back} alt="" /></div>
-      <div><img src={back} alt="" /></div>
-      <div><img src={back} alt="" /></div>
-      <div><img src={back} alt="" /></div>
+      <div className={styles.plamat}>
+        <div><img src={back} alt="" /></div>
+        <div><img src={back} alt="" /></div>
+        <div><img src={back} alt="" /></div>
+        <div><img src={back} alt="" /></div>
+        <div><img src={back} alt="" /></div>
+      </div>
+      
     </div>
   )
 }
