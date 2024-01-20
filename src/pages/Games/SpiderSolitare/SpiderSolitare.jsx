099317de87d849ac5b9df5833804758a1dc50c61
Zachary Kuchar <zacharyk323@live.com>
2024-01-20 12:56:01 -0500

    Adding to setup of styles with solitare

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -7,12 +7,43 @@ function SpiderSolitare() {
   return (
     <div className={styles.SpiderSolitare}>
       <h1>Spider Solitare</h1>
-      <div className={styles.plamat}>
-        <div><img src={back} alt="" /></div>
-        <div><img src={back} alt="" /></div>
-        <div><img src={back} alt="" /></div>
-        <div><img src={back} alt="" /></div>
-        <div><img src={back} alt="" /></div>
+      <div className={styles.playmat}>
+        <div className={styles.row}>
+          <img src={back} alt="1" />
+          <img src={back} alt="2" />
+          <img src={back} alt="3" />
+          <img src={back} alt="1" />
+        </div>
+        <div className={styles.row}>
+          <img src={back} alt="" />
+          <img src={back} alt="2" />
+          <img src={back} alt="2" />
+          <img src={back} alt="2" />
+        </div>
+        <div className={styles.row}>
+          <img src={back} alt="" />
+          <img src={back} alt="3" />
+          <img src={back} alt="3" />
+          <img src={back} alt="1" />
+        </div>
+        <div className={styles.row}>
+          <img src={back} alt="" />
+          <img src={back} alt="" />
+          <img src={back} alt="" />
+          <img src={back} alt="" />
+        </div>
+        <div className={styles.row}>
+          <img src={back} alt="" />
+          <img src={back} alt="3" />
+          <img src={back} alt="3" />
+          <img src={back} alt="3" />
+        </div>
+        <div className={styles.row}>
+          <img src={back} alt="" />
+          <img src={back} alt="5" />
+          <img src={back} alt="5" />
+          <img src={back} alt="5" />
+        </div>
       </div>
       
     </div>
