196e94f05897607781355f4d08c8a69a0ba40e56
Zachary Kuchar <zacharyk323@live.com>
2024-02-06 20:08:57 -0500

    Initial setup for war rules

--- src/pages/Games/War/War.jsx
+++ src/pages/Games/War/War.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import back from '../../../Images/BACKCARD.JPG'
 
 function War() {
+    const [rules, setRules] = useState(false)
+
+    const newGame = () =>{
+      console.log("New war game")
+    }
+
   return (
     <div>
+      <div>
+        <button onClick={newGame}>New game</button>
+        <button onClick={()=>{setRules(!rules)}}>Show rules</button>
+      </div>
+            {
+        rules &&
+        <div>
+          <ul>
+            <li>Each player starts with 7 cards in hand</li>
+          </ul>
+        </div>
+      }
       <h1>War</h1>
       <h2>Com</h2>
       <img src={back} alt="" />
