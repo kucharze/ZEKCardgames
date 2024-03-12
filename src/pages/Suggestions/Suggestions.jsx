ac5f19908910f98423290eff297949a21aaee172
Zachary Kuchar <zacharyk323@live.com>
2024-03-12 19:28:36 -0400

    Added styles for suggestions page

--- src/pages/Suggestions/Suggestions.jsx
+++ src/pages/Suggestions/Suggestions.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Suggestions.module.css'
 
 function Suggestions() {
   return (
-    <div>
+    <div className={styles.Suggestions}>
       <h1>Suggestions</h1>
       <h3>If you have any suggestions you would like to make or would like to report a bug
         You may submit them down below.
