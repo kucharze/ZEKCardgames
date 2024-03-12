642340066ce1a388eb7803db19e610088905e2b9
Zachary Kuchar <zacharyk323@live.com>
2024-03-12 19:23:15 -0400

    Spelling fix

--- src/pages/Suggestions/Suggestions.jsx
+++ src/pages/Suggestions/Suggestions.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Suggestions() {
   return (
     <div>
-      <h1>Suggesstions</h1>
+      <h1>Suggestions</h1>
       <h3>If you have any suggestions you would like to make or would like to report a bug
         You may submit them down below.
       </h3>
