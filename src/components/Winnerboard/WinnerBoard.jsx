14821fac6dff1532d2469dcf26961e67c731052f
Zachary Kuchar <zacharyk323@live.com>
2024-04-23 19:02:47 -0400

    Enable import of Fireworks

--- src/components/Winnerboard/WinnerBoard.jsx
+++ src/components/Winnerboard/WinnerBoard.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 import styles from './WinnerBoard.module.css'
-// import Firework from '../Firework/Firework'
+import Firework from '../Firework/Firework'
 
 function WinnerBoard({winner}) {
   return (
     <div className={styles.WinnerBoard}>
       {winner && <h1>You win</h1>}
       {(!winner) && <h1>You lose</h1>}
-      {/* <Firework/> */}
+      {winner && <Firework/>}
       {/* <h2>Play again?</h2>
       <button>Yes</button>
       <button>No</button> */}
