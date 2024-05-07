198f68bcc6f0d968b95e47097a24e9a761a705b4
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-05-06 20:21:54 -0400

    Initial new game logic for matching

--- src/pages/Games/Matching/Matching.jsx
+++ src/pages/Games/Matching/Matching.jsx
@@ -8,12 +8,18 @@ function Matching() {
   const [el2, setEl2] = useState(null)
 
   const newGame = () =>{
+    console.log("A new game of mathing")
+    console.log("Load in pictures and randomize their positions")
 
+    setEl1(null)
+    setEl2(null)
   }
 
   return (
     <div className={styles.Matching}>
       <h1>Matching</h1>
+      <button onClick={newGame}>New game</button>
+
       <div className={styles.matchzone}>
         <img src={back} alt="" />
         <img src={back} alt="" />
