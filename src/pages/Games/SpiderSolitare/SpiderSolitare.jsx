99a05458b99121f77a02d8d28a3ee74e7a2fc701
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-05-08 18:17:25 -0400

    Initial logic for soliatre new game

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -4,15 +4,25 @@ import Deck from '../../../gamecomponents/Deck'
 import styles from './Spidersolitare.module.css'
 
 function SpiderSolitare() {
+  const [deck, setDeck] = useState(new Deck())
   const [row1, setRow1] = useState([])
   const [row2, setRow2] = useState([])
   const [row3, setRow3] = useState([])
   const [row4, setRow4] = useState([])
   const [row5, setRow5] = useState([])
+  
+  const newGame = () =>{
+    row1.push(deck.dealACard())
+    row2.push(deck.dealACard())
+    row3.push(deck.dealACard())
+    row4.push(deck.dealACard())
+    row5.push(deck.dealACard())
+  }
 
   return (
     <div className={styles.SpiderSolitare}>
       <h1>Spider Solitare</h1>
+      <button onClick={newGame}>New game</button>
       <div className={styles.playmat}>
         <div className={styles.row}>
           <img src={back} alt="1" />
