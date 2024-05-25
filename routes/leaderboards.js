8435b47458b27948b7028ae06ec3cc9bf140ad8a
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-05-24 20:22:42 -0400

    Initial leaderboard route settings

---
+++ routes/leaderboards.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  console.log("Server leaderboard test");
+  console.log(req.body);
+});
