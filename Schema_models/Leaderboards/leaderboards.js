7198447b9a757a23c27c994cb0df76a46fd164ca
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-05-30 14:22:29 -0400

    Change to server for leaderboards

--- Schema_models/Leaderboards/leaderboards.js
+++ Schema_models/Leaderboards/leaderboards.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
 
 // const Suggestion = require("../Schema_models/suggestions/suggestions");
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   console.log("Server Leaderboard test");
   console.log(req.body);
 });
