e9d8b0bc4fd5c103f179d293d48d85094494b4f1
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-05 11:13:44 -0400

    Adjusted client logic to send leaderboard option to server

--- src/pages/Leaderboards/Leaderboards.jsx
+++ src/pages/Leaderboards/Leaderboards.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import styles from './Leaderboards.module.css'
 import axios from 'axios'
 
@@ -6,22 +6,32 @@ function Leaderboards() {
   const [option,setOption] = useState("")
 
   const getLeaderboardData = async (e) =>{
+    if(option === 'Select a leaderboard option'){
+      alert("Please select a valid option")
+      return
+    }
+    
     e.preventDefault()
     console.log("Leaderboard server test")
     try {
-      let res = await axios.get("http://localhost:3001/leaderboards")
+      let res = await axios.get("http://localhost:3001/leaderboards/gofish")
 
       console.log(res)
     } catch (error) {
       console.log("There was an error",error)
     }
   }
+
+  useEffect(()=>{
+    console.log(option)
+  },[option])
+
   return (
     <div className={styles.Leaderboards}>
       <h1>Leaderboards</h1>
       <h2>Select a game to bring up the leaderboards relating to it.</h2>
       <form>
-        <select>
+        <select value={option} onChange={(e) =>{setOption(e.target.value)}}>
             <option>Select a leaderboard option</option>
             <option>Crazy Eights</option>
             <option>Blackjack</option>
