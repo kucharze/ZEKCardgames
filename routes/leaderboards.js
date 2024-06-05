9981bab7a97ab89b5e383045fbd4baafda0a091a
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-05 11:15:41 -0400

    Read in data to load a leaderboard

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 // const Suggestion = require("../Schema_models/suggestions/suggestions");
 
-router.get("/", async (req, res) => {
+router.get("/:game", async (req, res) => {
   console.log("Server Leaderboard test");
+  console.log("We are searching for game", req.params.game);
   res.status(200).json("Success accessing server for leaderboards");
 });
 
