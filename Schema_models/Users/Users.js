8ea634b8f3617b88edc3f691d147e6baa47ddb72
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-06 11:48:11 -0400

    Changes to userschema to match needed data

--- Schema_models/Users/Users.js
+++ Schema_models/Users/Users.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const User = mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, required: true },
-    post: { type: String, required: true },
-    likes: { type: Number, required: true },
+    name: { type: String, required: true },
+    userName: { type: String, required: true },
+    password: { type: String, required: true },
   }
   //   {
   //     timestamps: true, //Tells when the timestamp has been created or last updated
