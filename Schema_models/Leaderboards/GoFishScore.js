6170ba52477d1b35f8ba14e27dd5d038f692ae37
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-10 19:43:44 -0400

    Go Fish Score Schema

---
+++ Schema_models/Leaderboards/GoFishScore.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+// const User = require("./users");
+
+const GoFishScore = mongoose.Schema(
+  {
+    // user: { type: mongoose.Schema.Types.ObjectId, required: true },
+    Username: { type: String, required: true },
+    Score: { type: Number, required: true },
+  },
+  {
+    timestamps: true, //Tells when the timestamp has been created or last updated
+  }
+);
+
+module.exports = mongoose.model("GoFishScore", GoFishScore);
