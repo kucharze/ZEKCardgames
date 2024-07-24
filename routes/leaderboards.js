dc6cd6883482a0bc03fa178e470e6fb351ad19d1
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-07-24 11:05:17 -0400

    Initial logic to pull leaderboard data

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -8,7 +8,35 @@ const WarScore = require("../Schema_models/leaderboards/WarScore.js");
 router.get("/:game", async (req, res) => {
   console.log("Server Leaderboard test");
   console.log("We are searching for game", req.params.game);
-  res.status(200).json("Success accessing server for leaderboards");
+  //res.status(200).json("Success accessing server for leaderboards");
+
+  let game = req.params.game;
+
+  if (game === "Blackjack Wins") {
+    try {
+      const leaderboard = await Blackjack.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "Crazy Eights Moves") {
+    try {
+      const leaderboard = await EightMoves.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "War Score") {
+    try {
+      const leaderboard = await WarScore.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  }
 });
 
 module.exports = router;
