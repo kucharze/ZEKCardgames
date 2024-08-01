4bfba37b653d4749db9efaa2f5481b8da20258d6
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-08-01 16:31:09 -0400

    Post request for saving users

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,19 @@ router.get("/", async (req, res) => {
   res.status(200).json("Success accessing server for Users");
 });
 
+router.Post("/", async (req, res) => {
+  console.log("Saving a user");
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const user = new users(req.body);
+    user.save();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json("Error: " + error);
+  }
+
+  res.status(200).json("Success accessing server for Users");
+});
+
 module.exports = router;
