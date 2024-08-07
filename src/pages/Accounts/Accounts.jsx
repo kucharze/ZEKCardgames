e939299c74dc61406d36f3ac6571c2c69d4ab727
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-08-07 11:09:12 -0400

    Function to create an account

--- src/pages/Accounts/Accounts.jsx
+++ src/pages/Accounts/Accounts.jsx
@@ -3,13 +3,32 @@ import styles from './Accounts.module.css'
 import axios from 'axios'
 
 function Accounts() {
+
+  const createAccount = async () => {
+    try {
+      let res = await axios.post('http://localhost:3001/accounts',
+        {
+          name: "test",
+          username: "test",
+          password: "test",
+
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className={styles.Accounts}>
       <h3>Active User: No active user</h3>
       <h2>Create a Login to have your name added to Leaderboards</h2>
       <form>
         <input placeholder='Name'/>
-        <input placeholder='Screenname'/>
+        <input placeholder='Username'/>
         <input placeholder='Password'/>
         <input placeholder='Confirm password'/><br/>
         <input type='submit' value='Create an Account'/>
