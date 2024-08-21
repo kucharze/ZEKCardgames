383113310e676dcda57654e3d9eb9a80db653a37
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-08-21 11:36:38 -0400

    Comment change

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/*", (req, res, next) => {
   next();
 });
 
-//routes
+// Put API routes here
 app.use("/suggestions", require("./routes/suggestions"));
 app.use("/leaderboards", require("./routes/leaderboards"));
 app.use("/users", require("./routes/users"));
