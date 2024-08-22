81e220bf1d56ab6d3f168c367e852e4392acb610
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-08-22 15:12:02 -0400

    Setup app context for use in project

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AppContextProvider } from "./contexts/app_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AppContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppContextProvider>
   </React.StrictMode>
 );
 
