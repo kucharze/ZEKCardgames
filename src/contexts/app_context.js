12ff8fdbd459680d75b63de324bb99fd66b78bda
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-09-03 09:18:37 -0400

    Initial logic to send leaderboard data to server

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -9,6 +9,27 @@ export const AppContextProvider = ({ children }) => {
   //Add functions for uploading to leaderboards
   const uploadToLeaderboards = (board) => {
     console.log("uploading to leaderboards");
+    if (board === "Select a leaderboard option") {
+      alert("Please select a valid option");
+      return;
+    }
+
+    try {
+      let res = axios.post(
+        "http://localhost:3001/leaderboards/" + board,
+        {
+          name: user,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(res);
+      setUser(res.data);
+    } catch (error) {}
   };
 
   return (
