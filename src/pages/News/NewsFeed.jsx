e68a20cd07f83bf652e0a6a5edaff86e685a84f6
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-09-11 12:39:52 -0400

    Changes to elements and styles

--- src/pages/News/NewsFeed.jsx
+++ src/pages/News/NewsFeed.jsx
@@ -5,7 +5,14 @@ function NewsFeed(darkmode) {
 
     return (
       <div className={styles.NewsFeed}>
-        <h1>Newfeed component</h1>
+        <div className={styles.Content}>
+            <h1>Newfeed component</h1>
+        <p>Welcome to our website.  Here you can see news about things that we are working on.</p>
+        <ol>
+            <li>Crazy Eights</li>
+        </ol>
+        </div>
+        
       </div>
     )
   
