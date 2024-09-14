7e6d18988c3d0269b2202880833c1a5ac4d77a28
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-09-14 19:16:49 -0400

    Added new function to check player hand for a card

--- src/pages/Games/GoFish/GoFish.jsx
+++ src/pages/Games/GoFish/GoFish.jsx
@@ -80,6 +80,26 @@ function GoFish({darkmode}) {
     comTurn()
   }
 
+  const testPlayHand = (card) =>{
+    //Check Player hand for a given card
+    console.log("Testing play hand")
+    let i = 0;
+    let tempHand = playHand
+    let stillGiving = false
+
+    while(i<tempHand.length){
+        //Check if the card the computer is asking for is still in the player's hand
+        console.log(tempHand[i])
+        if(tempHand[i].value===card.value){
+          stillGiving = true
+          break
+        }
+        i++
+    }
+    return stillGiving
+    
+  }
+
   const checkPlayHand = (card) =>{
     console.log("Checking play hand")
     let i = 0;
