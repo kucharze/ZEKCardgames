230aed6ccfbb41e264c9ee0b6845262f22a1c831
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-10-10 11:28:58 -0400

    Rules style changes and rules addition to matching

--- src/pages/Games/Matching/Matching.jsx
+++ src/pages/Games/Matching/Matching.jsx
@@ -6,6 +6,7 @@ function Matching() {
   const [matchList, setMatchList] = useState([])//List of elements that will get matched
   const [el1, setEl1] = useState(null)
   const [el2, setEl2] = useState(null)
+  const [rules, setRules] = useState(false)
 
   const newGame = () =>{
     console.log("A new game of mathing")
@@ -18,7 +19,23 @@ function Matching() {
   return (
     <div className={styles.Matching}>
       <h1>Matching</h1>
-      <button onClick={newGame}>New game</button>
+            <div>
+        <button onClick={newGame}>New game</button>
+        <button onClick={()=>{setRules(!rules)}}>Show rules</button>
+      </div>
+            {
+        rules &&
+        <div className={styles.rules}>
+          <ul>
+            <li>Each player starts with 7 cards in hand</li>
+            <li>Each player can ask the other if they have a given card in their hand</li>
+            <li>IF they have the asked for cards, they give them all to you</li>
+            <li>If they don't, you instead draw a card</li>
+            <li>Score a point if you have 4 copies of each card</li>
+             <li>Once the deck is out of cards, the player with the most points wins</li>
+          </ul>
+        </div>
+      }
 
       <div className={styles.matchzone}>
         <img src={back} alt="" />
