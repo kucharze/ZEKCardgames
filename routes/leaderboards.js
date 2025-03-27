192505815ed74c954c6bd842af753d752bff03eb
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-03-27 12:53:20 +0000

    leaderboard server function to find user's ranking

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -68,4 +68,64 @@ router.get("/:game", async (req, res) => {
   }
 });
 
+router.get("/:game/:user", async (req, res) => {
+  console.log("Server Leaderboard test");
+  console.log("We are searching for game", req.params.game);
+  //res.status(200).json("Success accessing server for leaderboards");
+
+  let game = req.params.game;
+
+  if (game === "Blackjack Wins") {
+    try {
+      const leaderboard = await Blackjack.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "Crazy Eights Moves") {
+    try {
+      const leaderboard = await EightMoves.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "War Score") {
+    try {
+      const leaderboard = await WarScore.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "Go Fish High Score") {
+    try {
+      const leaderboard = await GoFishScore.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "Matching Moves") {
+    try {
+      const leaderboard = await MatchMoves.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else if (game === "Spider Solitare Score") {
+    try {
+      const leaderboard = await SpiderSolitareScore.find();
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  } else {
+    res.status(400).json("Game Leaderboard not yet implemented");
+  }
+});
+
 module.exports = router;
