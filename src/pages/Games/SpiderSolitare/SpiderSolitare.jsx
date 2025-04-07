cc4d0d92332fa6d466ccccc20e6890d7bcdcda0a
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-04-07 00:30:26 +0000

    Fix to moving cards

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -181,23 +181,23 @@ function SpiderSolitare() {
      checkForPoint(row1)
     }
     else if(endrow===row2){
-      setRow1([...row2,...movelist])
+      setRow2([...row2,...movelist])
       checkForPoint(row2)
     }
     else if(endrow===row3){
-      setRow1([...row3,...movelist])
+      setRow3([...row3,...movelist])
       checkForPoint(row3)
     }
     else if(endrow===row4){
-      setRow1([...row4,...movelist])
+      setRow4([...row4,...movelist])
       checkForPoint(row4)
     }
     else if(endrow===row5){
-      setRow1([...row5,...movelist])
+      setRow5([...row5,...movelist])
       checkForPoint(row5)
     }
     else if(endrow===row6){
-      setRow1([...row6,...movelist])
+      setRow6([...row6,...movelist])
       checkForPoint(row6)
     }
 
