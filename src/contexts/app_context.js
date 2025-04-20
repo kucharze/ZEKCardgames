be4636aa408cf6ffa0aaef38843b1636cc00f4f2
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-04-20 17:20:24 +0000

    Commented out code that is no longer necessary

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -20,27 +20,27 @@ export const AppContextProvider = ({ children }) => {
         break;
       case "Blackjack Wins":
         URL = "http://localhost:3001/leaderboards/BlackjackWins"
-        blackjackWins(score);
+        // blackjackWins(score);
         break;
       case "Snip Snap Snorum":
         URL = "http://localhost:3001/leaderboards/SnipSnapSnorumTime"
-        snipSnipSnorumTime();
+        // snipSnipSnorumTime();
         break;
       case "Matching Moves":
         URL = "http://localhost:3001/leaderboards/MatchingMoves"
-        MatchingMoves(score)
+        // MatchingMoves(score)
         break;
       case "Go Fish High Score":
         URL = "http://localhost:3001/leaderboards/GoFishScore"
-        GoFishScore(score)
+        // GoFishScore(score)
         break;
       case "Spider Solitare Score":
         URL = "http://localhost:3001/leaderboards/SpiderSolitareScore"
-        SpiderSolitareScore(score)
+        // SpiderSolitareScore(score)
         break;
       case "War Score":
         URL = "http://localhost:3001/leaderboards/WarScore"
-        warScore(score)
+        // warScore(score)
         break;
       default:
         alert("No valid leaderboard option");
@@ -52,7 +52,7 @@ export const AppContextProvider = ({ children }) => {
         URL,
         {
           username: user,
-          score: moves,
+          score: score,
         },
         {
           headers: {
@@ -68,152 +68,152 @@ export const AppContextProvider = ({ children }) => {
      
   };
 
-  const eightMoves = (moves) => {
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/CrazyEightsMoves",
-        {
-          username: user,
-          score: moves,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const eightMoves = (moves) => {
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/CrazyEightsMoves",
+  //       {
+  //         username: user,
+  //         score: moves,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const snipSnipSnorumTime = (time) => {
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/SnipSnapSnorumTime",
-        {
-          username: user,
-          score: time,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const snipSnipSnorumTime = (time) => {
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/SnipSnapSnorumTime",
+  //       {
+  //         username: user,
+  //         score: time,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const blackjackWins = (moves) => {
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/BlackjackWins",
-        {
-          username: user,
-          score: moves,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const blackjackWins = (moves) => {
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/BlackjackWins",
+  //       {
+  //         username: user,
+  //         score: moves,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const MatchingMoves = (moves) =>{
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/MatchingMoves",
-        {
-          username: user,
-          score: moves,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const MatchingMoves = (moves) =>{
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/MatchingMoves",
+  //       {
+  //         username: user,
+  //         score: moves,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  const GoFishScore = (score) =>{
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/GoFishScore",
-        {
-          username: user,
-          score: score,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const GoFishScore = (score) =>{
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/GoFishScore",
+  //       {
+  //         username: user,
+  //         score: score,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  const warScore = (score) =>{
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/WarScore",
-        {
-          username: user,
-          score: score,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const warScore = (score) =>{
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/WarScore",
+  //       {
+  //         username: user,
+  //         score: score,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  const SpiderSolitareScore = (score) =>{
-    try {
-      let res = axios.post(
-        "http://localhost:3001/leaderboards/SpiderSolitareScore",
-        {
-          username: user,
-          score: score,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(res);
-      alert("Leaderboard upload successful")
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const SpiderSolitareScore = (score) =>{
+  //   try {
+  //     let res = axios.post(
+  //       "http://localhost:3001/leaderboards/SpiderSolitareScore",
+  //       {
+  //         username: user,
+  //         score: score,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(res);
+  //     alert("Leaderboard upload successful")
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return (
     <AppContext.Provider
