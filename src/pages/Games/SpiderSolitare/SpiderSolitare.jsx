066a44d431b78d1184c894a609efbcbd74baa6bf
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-05-12 00:03:33 +0000

    Removed commented out function

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -178,39 +178,6 @@ function SpiderSolitare() {
         }
   }
 
-  // //Run checks to see if we should move one face card to another
-  // const checkFaceMove = (mover, target) =>{
-  //   //Target is an ace
-  //   if(target.value === "A"){
-  //     //This can never be true, ace is the lowest number
-  //     return false
-  //   }
-  //   //Target is a king
-  //   else if(target.value === "k"){
-  //     //King is the highest value, this is always true
-  //     return true
-  //   }
-  //   //Target is a queen
-  //   else if(target.value === "q"){
-  //     if(mover.value === "q" || mover.value === "k"){
-  //       return true
-  //     }
-  //     else{
-  //       return false
-  //     }
-  //   }
-  //   //Target is a jack
-  //   //Return true if ace otherwise return false
-  //   else if(target.value === "j"){
-  //     if(mover.value === 'a'){
-  //       return true
-  //     }
-  //     else{
-  //       return false
-  //     }
-  //   }
-  // }
-
   const setRow = (row) =>{
     if(row===row1){
       setMoveRow(row1)
