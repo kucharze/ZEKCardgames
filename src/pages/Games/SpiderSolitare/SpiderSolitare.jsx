2e4e62b4cd8b8d5c4df2578f8f3a4eb2cefb0299
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2025-09-05 14:34:19 -0400

    Removed dead code

--- src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
+++ src/pages/Games/SpiderSolitare/SpiderSolitare.jsx
@@ -60,48 +60,30 @@ function SpiderSolitare({darkMode}) {
     {
       row1[row1.length-1].back = false
     }
-    else{
-      deployEmpty(row1)
-    }
     
     if(row2.length!==0)
     {
       row2[row2.length-1].back = false
-    }
-    else{
-      deployEmpty(row2)
     }
     
     if(row3.length!==0)
     {
       row3[row3.length-1].back = false
     }
-    else{
-      deployEmpty(row3)
-    }
     
     if(row4.length!==0)
     {
       row4[row4.length-1].back = false
-    }
-    else{
-      deployEmpty(row4)
     }
     
     if(row5.length!==0)
     {
      row5[row5.length-1].back = false
     }
-    else{
-      deployEmpty(row5)
-    }
     
     if(row6.length!==0)
     {
       row6[row6.length-1].back = false
-    }
-    else{
-      deployEmpty(row6)
     }
     
   }
