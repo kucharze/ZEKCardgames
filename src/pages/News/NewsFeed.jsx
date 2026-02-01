69345d57f9c2486239cf75bcf6359fd5f84e2dc2
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-02-01 00:35:21 +0000

    Text adjustment

--- src/pages/News/NewsFeed.jsx
+++ src/pages/News/NewsFeed.jsx
@@ -7,7 +7,8 @@ function NewsFeed(darkmode) {
       <div className={styles.NewsFeed}>
         <div className={styles.Content}>
             <h1>Newfeed component</h1>
-        <p>Welcome to our website.  Here you can see news about things that we are working on.</p>
+        <p>Welcome to our website.</p>
+          <p>Here you can see news about things that we are working on.</p>
         <ol>
             <li>Crazy Eights - Done</li>
             <li>Snip Snap Snorum - Done</li>
