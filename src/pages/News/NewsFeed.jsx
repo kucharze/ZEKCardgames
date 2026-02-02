e06feba8a7c6a76e5020b3a703486ed1576c88f0
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2026-02-01 20:42:43 -0500

    Updated news page with new statuses

--- src/pages/News/NewsFeed.jsx
+++ src/pages/News/NewsFeed.jsx
@@ -8,7 +8,7 @@ function NewsFeed(darkmode) {
         <div className={styles.Content}>
             <h1>Newfeed component</h1>
         <p>Welcome to our website.</p>
-          <p>Here you can see news about things that we are working on.</p>
+        <p>Here you can see news about things that we are working on.</p>
         <ol>
             <li>Crazy Eights - Done</li>
             <li>Snip Snap Snorum - Done</li>
@@ -20,6 +20,16 @@ function NewsFeed(darkmode) {
             <li>Online game features - TODO</li>
             <li>Load Leaderboard - DONE</li>
             <li>Upload to Leaderboards - TODO</li>
+            <li><ol>
+                  <li>Crazy Eights - In Progress</li>
+                  <li>Snip Snap Snorum - In Progress</li>
+                  <li>Black jack - In Progress</li>
+                  <li>Matching - In Progress</li>
+                  <li>Go Fish - In Progress</li>
+                  <li>Spider Solitare - In Progress</li>
+                  <li>War - In Progress</li>
+                </ol>
+            </li>
             <li>Login functions - Done</li>
             <li>Suggestions - DONE</li>
         </ol>
